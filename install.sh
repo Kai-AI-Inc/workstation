9122883febf2b472a309a1a5d69e3222bbdcd474
Yossi Shmueli <ysa.angel@gmail.com>
2022-04-24 09:58:15 +0300

    fix(install): fix item install

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 usage (){
   echo "Usage: ./install.sh -u|--git-user-name <YOUR-GIT-USER-NAME> -e|--git-email <YOUR-GIT-EMAIL> -t|--github-packages-token <GITHUB-PACKAGES-TOKEN> [-x|--exclude <comma separated string for apps that will not be installed>]"
   echo "Usage: Github packages token can be fetched from: github.com => settings => developer settings => personal access tokens"
-  echo "Usage: Available excludes: whatsapp,telegram,visual-studio-code,github,gh,datagrip,atom"
+  echo "Usage: Available excludes: whatsapp,telegram,visual-studio-code,github,gh,datagrip,atom,iterm2"
 }
 
 if [ -z "${GIT_USER_NAME}" ] || [ -z "${GIT_EMAIL}" ] || [ -z "${GITHUB_PACKAGES_TOKEN}" ]; then
@@ -88,7 +88,7 @@ echo "Make sure to edit .zshrc with t"
 
 # Applications
 brew install --cask notion
-install iterm --cask
+install iterm2 --cask
 
 # Dev Apps
 brew install --cask webstorm
